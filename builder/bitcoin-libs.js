815316a956af0ef29c4fbd329f4c5d96d24b1aef
Kobi Gurkan <kobigurk@gmail.com>
2015-08-18 10:00:08 +0300

    node modules for build

--- builder/bitcoin-libs.js
+++ builder/bitcoin-libs.js
@@ -1,9 +1,9 @@
 var bitcoinlibs = {
-      base58: require('bs58'),
-      bitcoin: require('bitcoinjs-lib'),
-        ecurve: require('ecurve'),
-            BigInteger: require('bigi'),
-              Buffer: require('buffer')
-}
+    base58: require('bs58'),
+    bitcoin: require('bitcoinjs-lib'),
+    ecurve: require('ecurve'),
+    BigInteger: require('bigi'),
+    Buffer: require('buffer')
+};
 
-module.exports = bitcoinlibs
+module.exports = bitcoinlibs;
